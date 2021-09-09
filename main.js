8554d12553d527bb2e74961d880e1183c68fe534
Ngo Quoc Hieu <quochieutb12@gmail.com>
2021-09-10 02:39:43 +0700

    change name img - > item-img-container /add .img /.item-img-container.img/fixing btn-buy

--- main.js
+++ main.js
@@ -4,6 +4,7 @@ const jsItem = document.querySelector('.js-item');
 
 
 jsItem.addEventListener('click', () => {
-    Object.values(jsSubItem.classList).includes('open') || "" ? jsSubItem.classList.remove('open') :jsSubItem.classList.add('open');   
+    jsSubItem.classList.contains('open') || "" ? jsSubItem.classList.remove('open') :jsSubItem.classList.add('open')
+    // Object.values(jsSubItem.classList).includes('open') || "" ? jsSubItem.classList.remove('open') :jsSubItem.classList.add('open');   
 })
 
